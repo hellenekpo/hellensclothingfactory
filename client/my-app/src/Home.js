ca2a325707fdd48417667fb7ab30fba9ee46c38d
Hellen Ekpo <he560@nyu.edu>
2023-07-12 20:21:27 -0400

    Formatting

--- client/my-app/src/Home.js
+++ client/my-app/src/Home.js
@@ -1,52 +1,38 @@
 import React from 'react';
 import './Home.css';
-import { useState } from 'react';
-import axios from 'axios';
 import styled from 'styled-components'
 import { desktop, tablet, mobile } from './responsive';
-import { Link } from 'react-router-dom'
-import HomeNav from './HomeNav';
-import Slide from './Slide.js';
-import { Admin, Menu, Reload, Resize, Search } from '@rsuite/icons';
-import { IconButton } from "rsuite";
-import BrandSlider from './BrandSlider';
 import * as AWS from 'aws-sdk';
-import newlogo from './images/newlogo.png'
-import welcomeimage from './images/welcomeimage.png'
-import aboutusfolderopened from './images/aboutusfolderopened.png'
-import shopfolderclosed from './images/shopfolderclosed.png'
-import contactfolderopen from './images/contactfolderopen.png'
 import newlogo1 from './images/newlogo1.png'
 import signup from './images/signup.png'
-import aboutusfolderclosed from './images/aboutusfolderclosed.png'
-import shopfolderopened from './images/shopfolderopened.png'
-import contactfolderclosed from './images/contactfolderclosed.png'
 import config from './config.json'
-import {Routes, Route, useNavigate} from 'react-router-dom';
 AWS.config.update(config);
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 const Container = styled.div`
+  display: flex;
+  justify-content: center;
 `;
 
 const ComingSoon = styled.img`
-	width: 70%;
-    height: 70%;
-	display: block;
-  	margin-left: auto;
-    margin-top: 10%;
-  	margin-right: auto;
+  max-width:100%;
+  max-height:40%;
+  position:absolute;
+  top:50%;
+  left:50%;
+  transform:translate(-50%,-100%);
 
     ${desktop({display: "block",})}
     ${tablet({display:"block"})}
     ${mobile({display: "block",})}
 `;
 const SignUpForUpdates = styled.img`
-	width: 70%;
-    height: 70%;
-	display: block;
-  	margin-left: auto;
-  	margin-right: auto;
+  max-width:100%;
+  max-height:40%;
+  position:absolute;
+  top:50%;
+  left:50%;
+  transform:translate(-50%,-0%);
 
     ${desktop({display: "block",})}
     ${tablet({display:"block"})}
@@ -58,7 +44,7 @@ const Home = () => {
   return (
     <Container>
 		<ComingSoon src={newlogo1} alt="comingsoon"/>
-		<SignUpForUpdates src={signup} alt="comingsoon2"/>
+        <SignUpForUpdates src={signup} alt="signup"/>
     </Container>
   )
 }
