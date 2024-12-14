59fd23e42ce0dae655702a96fc64c33fc10fcae4
Vic <victor.ekpo@gmail.com>
2024-12-13 23:11:55 -0600

    fade out to 15 seconds

--- client/my-app/src/components/TimedPopup/TimedPopup.js
+++ client/my-app/src/components/TimedPopup/TimedPopup.js
@@ -3,7 +3,7 @@ import TimedPopupForm from "../TimedPopupForm/TimedPopupForm";
 import styled, { keyframes } from 'styled-components';
 
 const TIME_TO_SHOW_POPUP_IN_MS = 5000; // 5 seconds
-const TIME_TO_HIDE_POPUP_IN_MS = 20000; // 20 seconds
+const TIME_TO_HIDE_POPUP_IN_MS = 15000; // 15 seconds
 
 const TimedPopup = () => {
   const [showPopup, setShowPopup] = useState(false);
